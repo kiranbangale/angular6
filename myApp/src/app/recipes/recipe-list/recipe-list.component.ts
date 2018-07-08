40b42c725d034eb550191ef241f2124bb51c6934
Kiran Bangale <kiranbangale@Kirans-MacBook-Pro.local>
2018-07-08 09:57:39 +0530

    Basic setup

--- myApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ myApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,13 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'recipe description',
+      'A test recipe 1',
+      'recipe 1 description',
+      'https://d2zk0c148yxqn5.cloudfront.net/wp-content/uploads/2017/04/eggless-choco-lava-cake-recipe.jpg'
+    ),
+    new Recipe(
+      'A test recipe 2',
+      'recipe 2 description',
       'https://d2zk0c148yxqn5.cloudfront.net/wp-content/uploads/2017/04/eggless-choco-lava-cake-recipe.jpg'
     )
   ];
