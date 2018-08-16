1479766218304eb8b724ce304707ee14622084c4
kiranbangale <kiranbangale@gmail.com>
2018-08-16 21:04:23 +0530

    http client

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,8 +14,8 @@ import { CoreModule } from './core/core.module';
     AppComponent
   ],
   imports: [
-    BrowserModule, 
-    HttpModule, 
+    BrowserModule,
+    HttpClientModule,
     AppRoutingModule, 
     SharedModule, 
     ShoppingListModule, 
--- myApp/src/app/shared/data-storage.service.ts
+++ myApp/src/app/shared/data-storage.service.ts
@@ -1,35 +1,35 @@
-import { Http, Response } from "@angular/http";
-import { Injectable } from "@angular/core";
-import { RecipeService } from "../recipes/recipe.service";
-import { Recipe } from "../recipes/recipe.model";
-import { AuthService } from "../auth/auth.service";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {}
+    constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
     storeRecipes() {
         const token = this.authService.getToken();
-        return this.http.put('https://ng-recipe-book-f079a.firebaseio.com/recipes.json?auth='+token, this.recipeService.getRecipes());
+        return this.httpClient.put('https://ng-recipe-book-f079a.firebaseio.com/recipes.json?auth='+token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
         const token = this.authService.getToken();
 
-        this.http.get('https://ng-recipe-book-f079a.firebaseio.com/recipes.json?auth='+token)
+        this.httpClient.get<Recipe[]>('https://ng-recipe-book-f079a.firebaseio.com/recipes.json?auth='+token)
         .subscribe(
-            (response: Response) => {
-                const recipes: Recipe[] = response.json();
+            (recipes) => {
                 this.recipeService.setRecipes(recipes);
             }
-        ); 
+        );
     }
 
     // getRecipes() {
-    //     this.http.get('https://ng-recipe-book-f079a.firebaseio.com/recipes.json')
+    //     const token = this.authService.getToken();
+    //     this.httpClient.get<Recipe[]>('https://ng-recipe-book-f079a.firebaseio.com/recipes.json'+token)
     //     .map(
-    //         (response: Response) => {
-    //             const recipes: Recipe[] = response.json();
+    //         (recipes) => {
     //             for(let recipe of recipes) {
     //                 if(!recipe['ingredients']) {
     //                     recipe['ingredients'] = [];
@@ -42,6 +42,6 @@ export class DataStorageService {
     //         (recipes: Recipe[]) => {
     //             this.recipeService.setRecipes(recipes);
     //         }
-    //     ); 
+    //     );
     // }
 }
